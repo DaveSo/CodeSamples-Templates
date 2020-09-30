1d9db730f12b0a4d242d54a7accd34b57dc0e57e
David Šotola <sotoladavid@gmail.com>
2020-09-30 16:12:49 +0200

    Two sample projects.

---
+++ AutofacFirstUse/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+
+namespace AutofacFirstUse
+{
+    class Program
+    {
+        private static IContainer Container { get; set; }
+
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<Teacher>().As<IPerson>();
+            builder.RegisterType<Student>().As<IPerson>();
+            Container = builder.Build();
+
+            var persons = Container.Resolve<IEnumerable<IPerson>>();
+
+
+            foreach (var person in persons)
+            {
+                person.WriteClassName();
+            }
+        }
+    }
+
+    public interface IOutput
+    {
+        void Write(string content);
+    }
+
+    public class ConsoleOutput : IOutput
+    {
+        public void Write(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+
+
+    public interface IPerson
+    {
+        void WriteClassName();
+    }
+
+
+    public class Teacher : IPerson
+    {
+        private IOutput _output;
+        public Teacher(IOutput output)
+        {
+            _output = output;
+        }
+
+        public void WriteClassName()
+        {
+            _output.Write(GetType().Name);
+        }
+    }
+    public class Student : IPerson
+    {
+        /*private IOutput _output;
+        public Student(IOutput output)
+        {
+            _output = output;
+        }*/
+
+        public void WriteClassName()
+        {
+            Console.WriteLine(GetType().Name);
+        }
+    }
+}
---
+++ ParallelArrayVsConcurrentBag/ParallelArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks; 
+
+namespace ParallelArray
+{
+    public class Program
+    {
+        static Stopwatch watch = new System.Diagnostics.Stopwatch();
+        static void Main(string[] args)
+        {
+            pokus();
+        }
+
+        public static void pokus()
+        {
+            int cykluu = 100000;
+
+            watch.Start();
+            {
+              //  var pole = new List<string>();
+                var pole2 = new ConcurrentBag<string>();
+
+                Parallel.For(0, cykluu, index =>
+                {
+                   // pole.Add("index"); 
+                    pole2.Add("index");
+                });
+
+                Console.WriteLine(
+                   // $"Počet záznamů v poli: {pole.Count}\n" +
+                                  $"Počet záznamů v poli2: {pole2.Count}" +
+                                  $"");
+            }
+            watch.Stop();
+            Console.WriteLine($"Zpracováno za: {watch.ElapsedMilliseconds} ms");
+             
+        }
+
+
+
+
+         
+    }
+}
